f392db3d08e266145609829203b3b2406d3c59a7
Michiel Kalkman <michiel@etryx.com>
2010-11-02 02:08:48 +0100

    Minor indentation cleanup

--- httpproxy.js
+++ httpproxy.js
@@ -42,21 +42,20 @@ net.createServer(function (socket) {
     socket.setEncoding("utf8");
     socket.on("data", function (data) {
         resolveHost(extractHost(data), function(host) { 
-          var stream = net.createConnection(80, host);
-          stream.on("connect", function() {
-              stream.write(data);
-            });
-          stream.on("data", function(resp) {
-              socket.write(resp);
-            });
-          stream.on("end", function() {
-              stream.end();
-            });
-        }
-      );
-    });
-    socket.on("end", function () {
-        socket.end();
+            var stream = net.createConnection(80, host);
+            stream.on("connect", function() {
+                stream.write(data);
+              });
+            stream.on("data", function(resp) {
+                socket.write(resp);
+              });
+            stream.on("end", function() {
+                stream.end();
+              });
+          });
       });
+  socket.on("end", function () {
+      socket.end();
+    });
   }).listen(LOCAL_PROXY_PORT, LOCAL_PROXY_HOST);
 
